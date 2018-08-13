3e086438b19fce66074e462336aade0c75a99cf9
Michael Youngstrom <youngstrom.m@husky.neu.edu>
2018-08-13 16:01:36 -0400

    Maintain ignore file tests without storing themn on platform

--- common/lib/xmodule/xmodule/modulestore/tests/utils.py
+++ common/lib/xmodule/xmodule/modulestore/tests/utils.py
@@ -1,6 +1,7 @@
 """
 Helper classes and methods for running modulestore tests without Django.
 """
+import os
 import random
 
 from contextlib import contextmanager, nested
@@ -70,6 +71,27 @@ def mock_tab_from_json(tab_dict):
     with plugin errors.
     """
     return tab_dict
+
+
+def add_temp_files_from_dict(file_dict, dir):
+    """
+    Takes in a dict formatted as: { file_name: content }, and adds files to directory
+    """
+    for file_name in file_dict:
+        with open("{}/{}".format(dir, file_name), "w") as opened_file:
+            content = file_dict[file_name]
+            if content:
+                opened_file.write(content)
+
+
+def remove_temp_files_from_list(file_list, dir):
+    """
+    Takes in a list of file names and removes them from dir if they exist
+    """
+    for file_name in file_list:
+        file_path = "{}/{}".format(dir, file_name)
+        if os.path.exists(file_path):
+            os.remove(file_path)
 
 
 class MixedSplitTestCase(TestCase):
@@ -469,6 +491,14 @@ MODULESTORE_SHORTNAMES = DIRECT_MS_SETUPS_SHORT + MIXED_MS_SETUPS_SHORT
 SHORT_NAME_MAP = dict(zip(MODULESTORE_SETUPS, MODULESTORE_SHORTNAMES))
 
 CONTENTSTORE_SETUPS = (MongoContentstoreBuilder(),)
+
+DOT_FILES_DICT = {
+    ".DS_Store": None,
+    ".example.txt": "BLUE",
+}
+TILDA_FILES_DICT = {
+    "example.txt~": "RED"
+}
 
 
 class PureModulestoreTestCase(TestCase):
